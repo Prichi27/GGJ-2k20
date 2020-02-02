29bfcf974bc4bb57145c14ce20bcf321ee6b65ad
ismail nassurally <ismaiiil@live.com>
2020-02-02 10:11:27 +0400

    Title and intro

--- Assets/Project/Scripts/pan_zoom.cs
+++ Assets/Project/Scripts/pan_zoom.cs
@@ -6,12 +6,18 @@ public class pan_zoom : MonoBehaviour
 {
     public GameObject main_camera;
     public GameObject switch_moveable;
-    public GameObject holo_switch;
-    public GameObject holo_room_box;
-    public GameObject plane_obj;
+    public GameObject flip;
+    public GameObject box;
+    public GameObject title;
 
     private Animator switch_animator;
     private Animation camera_anim;
+
+    public Material holo_mat;
+    public Material plain_mat;
+    public Material metal_mat;
+    private Material[] flip_mats;
+    private Material[] box_mats;
 
     private bool hasPlayedIntro;
     private int switch_counter = 0;
@@ -21,19 +27,41 @@ public class pan_zoom : MonoBehaviour
     {
         switch_animator = switch_moveable.GetComponent<Animator>();
         camera_anim = main_camera.GetComponent<Animation>();
+        
+    }
 
+    void setHoloMats() {
+        flip_mats = flip.GetComponent<Renderer>().materials;
+        box_mats = box.GetComponent<Renderer>().materials;
+        flip_mats[0] = holo_mat;
+        box_mats[0] = holo_mat;
+        box_mats[1] = holo_mat;
+        flip.GetComponent<Renderer>().materials = flip_mats;
+        box.GetComponent<Renderer>().materials = box_mats;
+    }
+
+    void setPlainMats()
+    {
+        flip_mats = flip.GetComponent<Renderer>().materials;
+        box_mats = box.GetComponent<Renderer>().materials;
+        flip_mats[0] = plain_mat;
+        box_mats[0] = plain_mat;
+        box_mats[1] = metal_mat;
+        flip.GetComponent<Renderer>().materials = flip_mats;
+        box.GetComponent<Renderer>().materials = box_mats;
     }
 
     IEnumerator blink_switch_off()
     {
-        
+
         //display the title and reset the switch to initial
         //display hologram temporarily on button
         switch_animator.SetBool("is_pressed", true);
-        holo_switch.SetActive(true);
+        setHoloMats();
         yield return new WaitForSeconds(0.50f);
         switch_animator.SetBool("is_pressed", false);
-        holo_switch.SetActive(false);
+        setPlainMats();
+        title.SetActive(true);
         switch_counter++;
     }
 
@@ -46,10 +74,10 @@ public class pan_zoom : MonoBehaviour
         camera_anim.Play();
         hasPlayedIntro = true;
         switch_animator.SetBool("is_pressed", true);
+        setHoloMats();
         yield return new WaitForSeconds(0.50f);
         switch_animator.SetBool("is_pressed", false);
-        holo_switch.SetActive(true);
-        holo_room_box.SetActive(true);
+
         switch_counter++;
     }
     private void OnMouseDown()
